842fe1b85ed44e7f4c257bd0dd6c43e1d278eb19
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-02-25 20:12:39 +0200

    Added three clock arms and discrete arms rotation script

---
+++ 3D-clock/Assets/Scripts/Clock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+
+    private const float degreesPerHour = 30f;
+    private const float degreesPerMinute = 6f;
+    private const float degreesPerSecond = 6f;
+
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+    public bool continuous;
+    
+    private void Update()
+    {
+        // hoursTransform.Rotate(new Vector3(0, hour * 30, 0));
+        hoursTransform.localRotation =   Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
+    }
+
+}
