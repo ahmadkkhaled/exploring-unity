273e2088b3043b214d3f3cc36301a74f2eb4bf2b
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-02-26 22:27:04 +0200

    Added periodic rendering and adjusted camera position

--- building-a-graph/Assets/Scripts/Graph.cs
+++ building-a-graph/Assets/Scripts/Graph.cs
@@ -4,26 +4,41 @@ public class Graph : MonoBehaviour
 {
     public Transform pointPrefab;
     [Range(10,100)] public int resolution = 10;
-
+    public Transform[] points;
     private void Awake()
     {
-
+        points = new Transform[resolution];
         Transform point;
         Vector3 position = Vector3.zero;
         float step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
 
-        for (int i=0; i<resolution; i++)
+        for (int i=0; i<points.Length; i++)
         {
             point = Instantiate(pointPrefab);
             // point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f); /// right is a static property -> Class.getX() where getX() is a static function
 
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x;
             point.localPosition = position;
 
             point.localScale = scale;
             point.SetParent(transform, false); /// transform is an inherited property
+
+            points[i] = point;
+        }
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i<points.Length; i++)
+        {
+            Transform point = points[i];
+
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+
+            point.localPosition = position;
+
         }
     }
 }
